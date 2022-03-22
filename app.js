716a9cdea3a8c9291ed1b2e0dc49607da3840557
Ariel <arielrodriguez0@outlook.com>
2022-03-22 02:32:22 -0600

    third commit

--- app.js
+++ app.js
@@ -1,19 +1,66 @@
 //Ariel vs Moises, pelea de Flechas.//
 
-function Jugador (nombre) {
+function Jugador (nombre) { // esta fuciona es el objeto que contiene los puntos de vida, y puntos especiales por defecto del jugador//
     this.nombre = nombre;
     this.pv = 100;
     this.sp = 100;
 
 
-    this.curar = function( jugadorObjetivo ){
+    this.curar = function( jugadorObjetivo ){ // esta funcion cura al jugador objetivo con 20 special points//
+        
+    if  (this.sp >=40 ) {
+        this.sp -= 40;
+        jugadorObjetivo.pv += 20;
+
+       }else {
+        console.info (this.nombre + 'no tiene sp');
+
+       }
+
+       this.estado(jugadorObjetivo);
+
+       
+        
+    }
+
+
+    this.tirarFlecha = function(jugadorObjetivo) {
+
+       if (jugadorObjetivo.pv > 40){
+
+        jugadorObjetivo.pv -= 40;
+
+       }
+       else{
+           jugadorObjetivo.pv = 0;
+           console.error(jugadorObjetivo.nombre + ' esta muerto');
+       }
+
+       
+
+
+    };
+
+  
+
+
+
+
+    this.estado = function( jugadorObjetivo) {
+
+        console.info(this);
+        console.info(jugadorObjetivo);
 
     }
-}
 
-var ariel = new Jugador('Ariel');
+};
+
+var ariel = new Jugador('Ariel');    // aqui se definen los nombres de los jugadores, con el New y los valores por defecto//
 var moises = new Jugador('Moises');
 
 
 console.log( ariel);
 console.log(moises);
+
+
+ariel.curar(moises);
