7513812fa6095f4a266953edc1ead7035f2a49a9
Ariel <arielrodriguez0@outlook.com>
2022-03-22 00:50:53 -0600

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+//Ariel vs Moises, pelea de Flechas.//
+
 function Jugador (nombre) {
     this.nombre = nombre;
     this.pv = 100;
     this.sp = 100;
+
+
+    this.curar = function( jugadorObjetivo ){
+
+    }
 }
 
-var Ariel = new 
+var ariel = new Jugador('Ariel');
+var moises = new Jugador('Moises');
+
+
+console.log( ariel);
+console.log(moises);
